8ff3453cc3091a10a0a96c65b1b346cc3b8549ee
jihyuk0414 <dealon2580@naver.com>
2024-08-21 15:21:28 +0900

    update:PaymentDAO

--- src/main/java/haircutmaven/Main.java
+++ src/main/java/haircutmaven/Main.java
@@ -2,7 +2,9 @@ package haircutmaven;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.text.ParseException;
 
+import haircutmaven.dao.PaymentDAO;
 import haircutmaven.util.DBConnection;
 import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
@@ -10,8 +12,16 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Main {
 	
-	public static void main(String[] args) throws IOException, SQLException {
+	public static void main(String[] args) throws IOException, SQLException, ParseException {
 		log.info(DBConnection.getConnection().toString());
+		PaymentDAO.InsertPayment(null, 10000, 1, 1, 2);
+		try {
+			log.info(PaymentDAO.GetPaymentListByDate("2024-08-21").toString());
+		} catch (IOException | ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		log.info(PaymentDAO.GetOnePaymentByCustomer(1).toString());
 	}
 }
  
