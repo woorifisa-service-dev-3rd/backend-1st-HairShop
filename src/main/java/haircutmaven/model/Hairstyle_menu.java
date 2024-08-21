46b2c648c8857e736b97337efd1e2b67321cd588
kh-0818 <lkh3179@naver.com>
2024-08-21 15:29:17 +0900

    hairstyle_menu dao,model 채우기

--- src/main/java/haircutmaven/model/Hairstyle_menu.java
+++ src/main/java/haircutmaven/model/Hairstyle_menu.java
@@ -1,11 +1,43 @@
 package haircutmaven.model;
 
-import lombok.Builder;
+import java.util.Arrays;
 
-@Builder
-public class Hairstyle_menu {
-	private int styleId; // 스타일 ID
-	private String hairstyle; // 헤어스타일
-	private int cost; // 비용
+public class Hairstyle_menu{
+	private String[] hairstyles;
+	private int[] costs;
+	private int[] style_ids;
+	
+	public Hairstyle_menu(int size){
+		hairstyles=new String[size];
+		costs=new int[size];
+		style_ids=new int[size];
+	}
+	public int getStyle_ids(int index) {
+		return style_ids[index];
+	}
+	public void setStyle_ids(int index, int style_id) {
+		this.style_ids[index] = style_id;
+	}
+	public String getHairstyle(int index) {
+		return hairstyles[index];
+	}
+	public void setHairstyle(int index, String hairstyle) {
+		this.hairstyles[index] = hairstyle;
+	}
+	public int getCost(int index) {
+		return costs[index];
+	}
+	public void setCost(int index, int cost) {
+		this.costs[index] = cost;
+	}
+	public int getSize() {
+        return hairstyles.length;
+    }
 
+	public String toString() {
+        return "HairstyleMenu{" +"style_ids="+Arrays.toString(style_ids)+
+                "hairstyles=" + Arrays.toString(hairstyles) +
+                ", costs=" + Arrays.toString(costs) +
+                '}';
+    }
 }
